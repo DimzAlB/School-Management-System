10d6c53504d8d8aa53687455f01a4ef72a3ce30c
DimzAlB <albertson.dimzon16@gmail.com>
2026-03-29 18:05:48 +0800

    Added FeeManagementForm UI

---
+++ SchoolManagementSystem/src/com/sms/FeeManagementForm.java
@@ -0,0 +1,11 @@
+package com.sms;
+
+import javax.swing.*;
+
+public class FeeManagementForm {
+    private JTextField textField1;
+    private JTextField textField2;
+    private JTextField textField3;
+    private JButton addRecordButton;
+    private JTable table1;
+}
