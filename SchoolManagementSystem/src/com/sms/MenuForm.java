253b7d97c4bdacb4ead42fa19a43dfb1dec274d3
Ryzel <rhyzelchinquimpo@gmail.com>
2026-03-30 21:51:29 -0700

    Added Login Form, Student Information, Classes, Teacher Schedule and Menu forms

---
+++ SchoolManagementSystem/src/com/sms/MenuForm.java
@@ -0,0 +1,48 @@
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class MenuForm extends JFrame {
+    private JPanel MenuPanel;
+    private JButton studentinfoButton;
+    private JButton classButton;
+    private JButton feesreportsButton;
+    private JButton teacherschedButton;
+
+    public MenuForm() {
+        setTitle("Western Institute of Technology");
+        setContentPane(MenuPanel);
+        setSize(300, 400);
+        setLocationRelativeTo(null);
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+
+        studentinfoButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                StudentInformation studinfo = new StudentInformation();
+                studinfo.setVisible(true);
+                dispose();
+            }
+        });
+        classButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                ClassesForm classform = new ClassesForm();
+                classform.setVisible(true);
+                dispose();
+            }
+        });
+        teacherschedButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                TeacherScheduleForm teachschedform = new TeacherScheduleForm();
+                teachschedform.setVisible(true);
+                dispose();
+            }
+        });
+    }
+
+    public static void main(String[] args) {
+        new MenuForm().setVisible(true);
+    }
+}
---
+++ SchoolManagementSystem/src/com/sms/UserLogin.java
@@ -0,0 +1,37 @@
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class UserLogin extends JFrame {
+    private JPanel UserLoginPanel;
+    private JTextField usernym;
+    private JButton ENTERButton;
+    private JTextField emeil;
+
+    public UserLogin() {
+        setTitle("Western Institute of Technology");
+        setContentPane(UserLoginPanel);
+        setSize(450,350);
+        setLocationRelativeTo(null);
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+
+        ENTERButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                String usernym1 = usernym.getText();
+                String email1 = emeil.getText();
+
+                if (usernym1.isEmpty() || email1.isEmpty()) {
+                    JOptionPane.showMessageDialog(UserLogin.this, "Please fill all fields.");
+                } else {
+                    MenuForm menu = new MenuForm();
+                    menu.setVisible(true);
+                    dispose();
+                }
+            }
+        });
+    }
+    public static void main(String[] args) {
+        new UserLogin().setVisible(true);
+    }
+}
